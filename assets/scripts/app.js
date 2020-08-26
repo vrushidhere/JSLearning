80a6ec88e41dd0b8b267020a6ca3afabab91d6ea
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-08-26 12:24:46 +0530

    let and constant difference

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+let firstName = "vrushi";
+firstName = "Vrushasen";
+
+const lastName = "Dhere";
+//lastName = "dhere"; // throws error in console that you can't change constants
+
+console.log(firstName + ' ' + lastName);
